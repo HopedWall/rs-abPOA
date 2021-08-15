fdaf0a3ed81dc9c990d5c7a5733b081b7d934cfb
Francesco Porto <francesco.porto97@gmail.com>
2021-08-15 18:22:10 +0200

    Small changes

--- src/main.rs
+++ src/main.rs
@@ -10,6 +10,8 @@ mod tests {
     use std::os::raw::{c_int, c_char};
     use std::alloc::alloc;
     use std::ops::Deref;
+    use core::slice;
+    use std::mem::MaybeUninit;
 
     #[test]
     fn test() {
@@ -60,15 +62,13 @@ mod tests {
                 4, 4, 4, 4,  4, 4, 4, 4,  4, 4, 4, 4,  4, 4, 4, 4
             ];
 
-
             // Get the number of input sequences
             let n_seqs: c_int = seqs.len() as c_int;
 
             // Create a Vec with the sequences' length
             let mut seq_lens_val : Vec<c_int> = seqs.iter().map(|s| s.len() as c_int).collect();
             let seq_lens: *mut c_int  = seq_lens_val.as_mut_ptr();
-
-            println!("seq lens: {:#?}", seq_lens_val);
+            //println!("seq lens: {:#?}", seq_lens_val);
 
             // Create a matrix (bseqs_val) where:
             // - each row represents an input sequence (rows can have different lengths)
@@ -80,28 +80,53 @@ mod tests {
                     .map(|c| *(_nt4_table).get(c as usize).unwrap())
                     .collect()
             }).collect();
-            println!("bseqs val val: {:#?}",bseqs_val_val);
+            //println!("bseqs val val: {:#?}",bseqs_val_val);
 
             let mut bseqs_val: Vec<*mut u8> = bseqs_val_val.iter_mut().map(|mut s| s.as_mut_ptr()).collect();
             let bseqs : *mut *mut u8 = bseqs_val.as_mut_ptr();
 
-            abpoa_msa(ab, abpt, n_seqs, ptr::null_mut(), seq_lens, bseqs, stdout,
-                      ptr::null_mut(), ptr::null_mut(), ptr::null_mut(),
-                      ptr::null_mut(), ptr::null_mut(), ptr::null_mut());
-
-            /*
+            // Now perform the alignment
             let cons_seq: *mut *mut *mut u8 = ptr::null_mut();
             let cons_c: *mut *mut *mut c_int = ptr::null_mut();
             let cons_l : *mut *mut c_int = ptr::null_mut();
-            let cons_n: *mut c_int = &mut 0;
+            let cons_n: *mut c_int = ptr::null_mut();
             let msa_seq: *mut *mut *mut u8 = ptr::null_mut();
-            let msa_l : *mut c_int = &mut 0;
+            let msa_l : *mut c_int = ptr::null_mut();
+            let out : *mut FILE = ptr::null_mut();
 
-            abpoa_msa(ab_poa, abpt, 10, ptr::null_mut(), seq_lens, bseqs,
-                      ptr::null_mut(),
+            abpoa_msa(ab, abpt, n_seqs, ptr::null_mut(), seq_lens, bseqs, ptr::null_mut(),
                       cons_seq, cons_c, cons_l,
                       cons_n, msa_seq, msa_l);
-            */
+
+            println!(">Multiple_sequence_alignment\n");
+            println!("MSA len is: {:#?}", msa_l);
+            println!("Cons n is: {:#?}", cons_n);
+
+            /*
+            for i in 0..n_seqs {
+                for j in 0..*msa_l {
+                    print!("[i:{}, j:{}]: ", i, j);
+                    println!("{:#?}", **msa_seq.add((i*n_seqs+j) as usize));
+                }
+                println!();
+            }
+             */
+
+            //let v = slice::from_raw_parts(msa_seq, *msa_l as usize).to_vec();
+            //println!("{:#?}",msa_seq);
+
+            //let result : *mut *mut u8 = *msa_seq;
+            //let result2 = &result;
+            /*
+            println!(">Multiple_sequence_alignment");
+            for i in 0..n_seqs {
+                print!("ACGTN-");
+                for j in 0..*msa_l {
+                    print!("{}", msa_seq[i][j])
+                }
+                println!();
+            }
+             */
         }
     }
 }
