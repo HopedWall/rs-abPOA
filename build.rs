e6641421861a40cab85f2788e360f980d2df461b
Francesco Porto <francesco.porto97@gmail.com>
2021-08-16 19:07:06 +0200

    Moved code to main

--- build.rs
+++ build.rs
@@ -1,6 +1,5 @@
 extern crate bindgen;
 
-use std::env;
 use std::path::PathBuf;
 
 fn main() {
--- src/main.rs
+++ src/main.rs
@@ -1,132 +1,96 @@
 use ab_poa::abpoa::*;
-use std::ptr::null;
+use std::ptr;
+use std::os::raw::c_int;
 
-fn main() {}
+fn main() {
+    unsafe {
+        let ab = abpoa_init();
+        let mut abpt = abpoa_init_para();
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-    use std::{mem, io, ptr};
-    use std::os::raw::{c_int, c_char};
-    use std::alloc::alloc;
-    use std::ops::Deref;
-    use core::slice;
-    use std::mem::MaybeUninit;
+        (*abpt).set_out_msa(1);
+        (*abpt).set_out_cons(1);
+        (*abpt).w = 6;
+        (*abpt).k = 9;
+        (*abpt).min_w = 10;
+        (*abpt).set_progressive_poa(1);
 
-    #[test]
-    fn test() {
-        unsafe {
-            let ab = abpoa_init();
-            let mut abpt = abpoa_init_para();
+        abpoa_post_set_para(abpt);
 
-            (*abpt).set_out_msa(1);
-            (*abpt).set_out_cons(1);
-            (*abpt).w = 6;
-            (*abpt).k = 9;
-            (*abpt).min_w = 10;
-            (*abpt).set_progressive_poa(1);
+        let seqs: Vec<&str> = [
+            "CGTCAATCTATCGAAGCATACGCGGGCAGAGCCGAAGACCTCGGCAATCCA",
+            "CCACGTCAATCTATCGAAGCATACGCGGCAGCCGAACTCGACCTCGGCAATCAC",
+            "CGTCAATCTATCGAAGCATACGCGGCAGAGCCCGGAAGACCTCGGCAATCAC",
+            "CGTCAATGCTAGTCGAAGCAGCTGCGGCAGAGCCGAAGACCTCGGCAATCAC",
+            "CGTCAATCTATCGAAGCATTCTACGCGGCAGAGCCGACCTCGGCAATCAC",
+            "CGTCAATCTAGAAGCATACGCGGCAAGAGCCGAAGACCTCGGCCAATCAC",
+            "CGTCAATCTATCGGTAAAGCATACGCTCTGTAGCCGAAGACCTCGGCAATCAC",
+            "CGTCAATCTATCTTCAAGCATACGCGGCAGAGCCGAAGACCTCGGCAATC",
+            "CGTCAATGGATCGAGTACGCGGCAGAGCCGAAGACCTCGGCAATCAC",
+            "CGTCAATCTAATCGAAGCATACGCGGCAGAGCCGTCTACCTCGGCAATCACGT"
+        ].to_vec();
 
-            //println!("abpt: {:#?}", *abpt);
+        let _nt4_table : [u8; 256] = [
+            4, 4, 4, 4,  4, 4, 4, 4,  4, 4, 4, 4,  4, 4, 4, 4,
+            4, 4, 4, 4,  4, 4, 4, 4,  4, 4, 4, 4,  4, 4, 4, 4,
+            4, 4, 4, 4,  4, 4, 4, 4,  4, 4, 4, 4,  4, 5 /*'-'*/, 4, 4,
+            4, 4, 4, 4,  4, 4, 4, 4,  4, 4, 4, 4,  4, 4, 4, 4,
+            4, 0, 4, 1,  4, 4, 4, 2,  4, 4, 4, 4,  4, 4, 4, 4,
+            4, 4, 4, 4,  3, 4, 4, 4,  4, 4, 4, 4,  4, 4, 4, 4,
+            4, 0, 4, 1,  4, 4, 4, 2,  4, 4, 4, 4,  4, 4, 4, 4,
+            4, 4, 4, 4,  3, 4, 4, 4,  4, 4, 4, 4,  4, 4, 4, 4,
+            4, 4, 4, 4,  4, 4, 4, 4,  4, 4, 4, 4,  4, 4, 4, 4,
+            4, 4, 4, 4,  4, 4, 4, 4,  4, 4, 4, 4,  4, 4, 4, 4,
+            4, 4, 4, 4,  4, 4, 4, 4,  4, 4, 4, 4,  4, 4, 4, 4,
+            4, 4, 4, 4,  4, 4, 4, 4,  4, 4, 4, 4,  4, 4, 4, 4,
+            4, 4, 4, 4,  4, 4, 4, 4,  4, 4, 4, 4,  4, 4, 4, 4,
+            4, 4, 4, 4,  4, 4, 4, 4,  4, 4, 4, 4,  4, 4, 4, 4,
+            4, 4, 4, 4,  4, 4, 4, 4,  4, 4, 4, 4,  4, 4, 4, 4,
+            4, 4, 4, 4,  4, 4, 4, 4,  4, 4, 4, 4,  4, 4, 4, 4
+        ];
 
-            abpoa_post_set_para(abpt);
+        // Get the number of input sequences
+        let n_seqs: c_int = seqs.len() as c_int;
 
-            let seqs: Vec<&str> = [
-                "CGTCAATCTATCGAAGCATACGCGGGCAGAGCCGAAGACCTCGGCAATCCA",
-                "CCACGTCAATCTATCGAAGCATACGCGGCAGCCGAACTCGACCTCGGCAATCAC",
-                "CGTCAATCTATCGAAGCATACGCGGCAGAGCCCGGAAGACCTCGGCAATCAC",
-                "CGTCAATGCTAGTCGAAGCAGCTGCGGCAGAGCCGAAGACCTCGGCAATCAC",
-                "CGTCAATCTATCGAAGCATTCTACGCGGCAGAGCCGACCTCGGCAATCAC",
-                "CGTCAATCTAGAAGCATACGCGGCAAGAGCCGAAGACCTCGGCCAATCAC",
-                "CGTCAATCTATCGGTAAAGCATACGCTCTGTAGCCGAAGACCTCGGCAATCAC",
-                "CGTCAATCTATCTTCAAGCATACGCGGCAGAGCCGAAGACCTCGGCAATC",
-                "CGTCAATGGATCGAGTACGCGGCAGAGCCGAAGACCTCGGCAATCAC",
-                "CGTCAATCTAATCGAAGCATACGCGGCAGAGCCGTCTACCTCGGCAATCACGT"
-            ].to_vec();
+        // Create a Vec with the sequences' length
+        let mut seq_lens_val : Vec<c_int> = seqs.iter().map(|s| s.len() as c_int).collect();
+        let seq_lens: *mut c_int  = seq_lens_val.as_mut_ptr();
+        //println!("seq lens: {:#?}", seq_lens_val);
 
-            let _nt4_table : [u8; 256] = [
-                4, 4, 4, 4,  4, 4, 4, 4,  4, 4, 4, 4,  4, 4, 4, 4,
-                4, 4, 4, 4,  4, 4, 4, 4,  4, 4, 4, 4,  4, 4, 4, 4,
-                4, 4, 4, 4,  4, 4, 4, 4,  4, 4, 4, 4,  4, 5 /*'-'*/, 4, 4,
-                4, 4, 4, 4,  4, 4, 4, 4,  4, 4, 4, 4,  4, 4, 4, 4,
-                4, 0, 4, 1,  4, 4, 4, 2,  4, 4, 4, 4,  4, 4, 4, 4,
-                4, 4, 4, 4,  3, 4, 4, 4,  4, 4, 4, 4,  4, 4, 4, 4,
-                4, 0, 4, 1,  4, 4, 4, 2,  4, 4, 4, 4,  4, 4, 4, 4,
-                4, 4, 4, 4,  3, 4, 4, 4,  4, 4, 4, 4,  4, 4, 4, 4,
-                4, 4, 4, 4,  4, 4, 4, 4,  4, 4, 4, 4,  4, 4, 4, 4,
-                4, 4, 4, 4,  4, 4, 4, 4,  4, 4, 4, 4,  4, 4, 4, 4,
-                4, 4, 4, 4,  4, 4, 4, 4,  4, 4, 4, 4,  4, 4, 4, 4,
-                4, 4, 4, 4,  4, 4, 4, 4,  4, 4, 4, 4,  4, 4, 4, 4,
-                4, 4, 4, 4,  4, 4, 4, 4,  4, 4, 4, 4,  4, 4, 4, 4,
-                4, 4, 4, 4,  4, 4, 4, 4,  4, 4, 4, 4,  4, 4, 4, 4,
-                4, 4, 4, 4,  4, 4, 4, 4,  4, 4, 4, 4,  4, 4, 4, 4,
-                4, 4, 4, 4,  4, 4, 4, 4,  4, 4, 4, 4,  4, 4, 4, 4
-            ];
+        // Create a matrix (bseqs_val) where:
+        // - each row represents an input sequence (rows can have different lengths)
+        // - each column represents a base as per the _nth4_table
+        // Bseqs is a Vec of pointers each pointing to one row
 
-            // Get the number of input sequences
-            let n_seqs: c_int = seqs.len() as c_int;
+        let mut bseqs_val_val : Vec<Vec<u8>> = seqs.into_iter().map(|s| {
+            s.chars()
+                .map(|c| *(_nt4_table).get(c as usize).unwrap())
+                .collect()
+        }).collect();
+        //println!("bseqs val val: {:#?}",bseqs_val_val);
 
-            // Create a Vec with the sequences' length
-            let mut seq_lens_val : Vec<c_int> = seqs.iter().map(|s| s.len() as c_int).collect();
-            let seq_lens: *mut c_int  = seq_lens_val.as_mut_ptr();
-            //println!("seq lens: {:#?}", seq_lens_val);
+        let mut bseqs_val: Vec<*mut u8> = bseqs_val_val.iter_mut().map(|s| s.as_mut_ptr()).collect();
+        let bseqs : *mut *mut u8 = bseqs_val.as_mut_ptr();
 
-            // Create a matrix (bseqs_val) where:
-            // - each row represents an input sequence (rows can have different lengths)
-            // - each column represents a base as per the _nth4_table
-            // Bseqs is a Vec of pointers each pointing to one row
+        // Now perform the alignment
+        let cons_seq: *mut *mut *mut u8 = ptr::null_mut();
+        let cons_c: *mut *mut *mut c_int = ptr::null_mut();
+        let cons_l : *mut *mut c_int = ptr::null_mut();
+        let cons_n: *mut c_int = ptr::null_mut();
+        let msa_seq: *mut *mut *mut u8 = ptr::null_mut();
+        let msa_l : *mut c_int = ptr::null_mut(); //&mut 0 as *mut c_int;
+        let out : *mut FILE = stdout;
 
-            let mut bseqs_val_val : Vec<Vec<u8>> = seqs.into_iter().map(|s| {
-                s.chars()
-                    .map(|c| *(_nt4_table).get(c as usize).unwrap())
-                    .collect()
-            }).collect();
-            //println!("bseqs val val: {:#?}",bseqs_val_val);
+        abpoa_msa(ab, abpt, n_seqs, ptr::null_mut(), seq_lens, bseqs, out,
+                  cons_seq, cons_c, cons_l,
+                  cons_n, msa_seq, msa_l);
 
-            let mut bseqs_val: Vec<*mut u8> = bseqs_val_val.iter_mut().map(|mut s| s.as_mut_ptr()).collect();
-            let bseqs : *mut *mut u8 = bseqs_val.as_mut_ptr();
-
-            // Now perform the alignment
-            let cons_seq: *mut *mut *mut u8 = ptr::null_mut();
-            let cons_c: *mut *mut *mut c_int = ptr::null_mut();
-            let cons_l : *mut *mut c_int = ptr::null_mut();
-            let cons_n: *mut c_int = ptr::null_mut();
-            let msa_seq: *mut *mut *mut u8 = ptr::null_mut();
-            let msa_l : *mut c_int = ptr::null_mut();
-            let out : *mut FILE = ptr::null_mut();
-
-            abpoa_msa(ab, abpt, n_seqs, ptr::null_mut(), seq_lens, bseqs, ptr::null_mut(),
-                      cons_seq, cons_c, cons_l,
-                      cons_n, msa_seq, msa_l);
-
-            println!(">Multiple_sequence_alignment\n");
-            println!("MSA len is: {:#?}", msa_l);
-            println!("Cons n is: {:#?}", cons_n);
-
-            /*
-            for i in 0..n_seqs {
-                for j in 0..*msa_l {
-                    print!("[i:{}, j:{}]: ", i, j);
-                    println!("{:#?}", **msa_seq.add((i*n_seqs+j) as usize));
-                }
-                println!();
+        for i in 0..n_seqs {
+            for j in 0..*msa_l {
+                println!("lol");
             }
-             */
-
-            //let v = slice::from_raw_parts(msa_seq, *msa_l as usize).to_vec();
-            //println!("{:#?}",msa_seq);
-
-            //let result : *mut *mut u8 = *msa_seq;
-            //let result2 = &result;
-            /*
-            println!(">Multiple_sequence_alignment");
-            for i in 0..n_seqs {
-                print!("ACGTN-");
-                for j in 0..*msa_l {
-                    print!("{}", msa_seq[i][j])
-                }
-                println!();
-            }
-             */
         }
+
+        //println!("out: {}", (*out));
+        //println!("Msa_seq: {:#?}", msa_seq);
     }
 }
